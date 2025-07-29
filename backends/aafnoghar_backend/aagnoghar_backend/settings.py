43fa2e921638b0f350d993753586d2aa434508f9
PyreX00 <Subedianish2022@gmail.com>
2025-07-29 10:37:18 +0545

    added user model and user manager

--- backends/aafnoghar_backend/aagnoghar_backend/settings.py
+++ backends/aafnoghar_backend/aagnoghar_backend/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'useraccount',
+    'property',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'useraccount.User'

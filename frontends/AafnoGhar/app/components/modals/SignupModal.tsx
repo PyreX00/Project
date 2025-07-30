e1d31551208b1db47f4a04a6e2ed20d40b8b95af
PyreX00 <Subedianish2022@gmail.com>
2025-07-30 10:24:35 +0545

    log in added

--- frontends/AafnoGhar/app/components/modals/SignupModal.tsx
+++ frontends/AafnoGhar/app/components/modals/SignupModal.tsx
@@ -1,32 +1,78 @@
 'use client'
 
 import Modal from "./Modal";
-import { useState } from 'react'
+import { useState } from 'react';
+import { useRouter } from "next/navigation";
 import CustomButton from "../forms/CustomButton";
 import useSignupModal from "@/app/hooks/useSignupModal";
+import apiService from "@/app/services/apiService";
+import { handleLogin } from "@/app/lib/actions";
 
 
 const SignupModal = () =>{ 
+    
+    const router = useRouter();
+    const[email,setEmail]= useState('');
+    const [password1, setPassword1] = useState('');
+    const [password2, setPassword2] = useState('');
+    const[errors,setErrors] = useState<string[]>([]);
 
-    const SignupModal = useSignupModal()
+
+    const signupModal = useSignupModal()
+
+    const submitSignup = async () => {
+        const formData = {
+            email: email,
+            password1: password1,
+            password2: password2,
+        }
+
+        const response = await apiService.post('/api/auth/register/', formData); 
+
+        if ( response.access){
+  
+            handleLogin(response.user.pk, response.access, response.refresh)
+
+            signupModal.close()
+
+            router.push('/')
+        } else {
+            const tmpErrors: string[] = Object.values(response).map((error: any) =>{
+                return error;
+            })
+
+            setErrors(tmpErrors);
+        }   
+         
+    }
 
     const content = (
+
         <>
         <h2 className="mb-6 text-2xl"> Welcome to AafnoGhar, please login</h2>
-        <form className="space-y-4"> 
-            <input type ="Email" placeholder="Email" className="w-full h-[54px] px-4 border bordery-gray-300 rounded-xl"/>
+        <form 
+            action={submitSignup}
+        className="space-y-4"> 
+            <input onChange={(e)=> setEmail(e.target.value)} type ="Email" placeholder="Email" className="w-full h-[54px] px-4 border bordery-gray-300 rounded-xl"/>
 
-            <input type ="Password" placeholder="password" className="w-full h-[54px] px-4 border bordery-gray-300 rounded-xl"/>
+            <input onChange={(e)=> setPassword1(e.target.value)} type ="Password" placeholder="password" className="w-full h-[54px] px-4 border bordery-gray-300 rounded-xl"/>
             
-            <input type ="Password" placeholder="password" className="w-full h-[54px] px-4 border bordery-gray-300 rounded-xl"/>
+            <input onChange={(e)=> setPassword2(e.target.value)} type ="Password" placeholder="password" className="w-full h-[54px] px-4 border bordery-gray-300 rounded-xl"/>
             
-            <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-                this is error message
-            </div>
+            {errors.map((error, index)=>{
+                return(
+                    <div 
+                    key ={`error_${index}`}
+                    className="p-5 bg-airbnb text-white rounded-xl opacity-80">
+                    {error}
+                    </div>
+
+                )
+            })}  
 
             <CustomButton 
             label={"Submit"}
-            onClick={()=>console.log("Clicked submit")}
+            onClick={submitSignup}
 
             />
         </form>
@@ -36,8 +82,8 @@ const SignupModal = () =>{
 
     return(
         <Modal 
-            isOpen = {SignupModal.isOpen}
-            close={SignupModal.close}
+            isOpen = {signupModal.isOpen}
+            close={signupModal.close}
             label="Sign up"
             content = {content} 
             />

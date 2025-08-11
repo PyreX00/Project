b79220b263cc39144691cae697c85b183d3a4a2e
PyreX00 <Subedianish2022@gmail.com>
2025-08-11 13:29:59 +0545

    production docker file

--- backends/aafnoghar_backend/aafnoghar_backend/settings.py
+++ backends/aafnoghar_backend/aafnoghar_backend/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = False
 
 # ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 ALLOWED_HOSTS = ["165.232.186.192"]
@@ -204,8 +204,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
